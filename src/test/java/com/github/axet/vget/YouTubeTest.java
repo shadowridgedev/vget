bc8af7ef2ecd3350434827294396bc2f0b7c972c
Alexey Kuznetsov <axet@me.com>
2014-02-17 21:15:29 +0400

    update url

--- src/test/java/com/github/axet/vget/YouTubeTest.java
+++ src/test/java/com/github/axet/vget/YouTubeTest.java
@@ -3,7 +3,7 @@ package com.github.axet.vget;
 public class YouTubeTest extends AppManagedDownload {
 
     public static void main(String[] args) {
-        AppManagedDownload.main(new String[] { "http://www.youtube.com/watch?v=aQRLSBUNupg", "/Users/axet/Downloads" });
+        AppManagedDownload.main(new String[] { "http://www.youtube.com/watch?v=KoUT0ZfPiFk", "/Users/axet/Downloads" });
     }
 
 }
